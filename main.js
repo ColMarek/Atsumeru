e22ef9036b5d1f2878087675b3db6d39b14268a2
ColMarek <colmarek254@gmail.com>
2020-01-10 17:12:55 +0300

    Quite when all windows are closed on MacOS

--- main.js
+++ main.js
@@ -40,9 +40,8 @@ app.on("ready", createWindow);
 app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  // DON'T DO THAT. QUIT WHEN ALL WINDOWS ARE CLOSED.
+  app.quit();
 });
 
 app.on("activate", () => {
